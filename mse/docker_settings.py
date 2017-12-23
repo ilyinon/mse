95fd5158acb8f89606c39af823935c8b041d4594
rawsik <Bubuntu>
2017-12-23 21:20:12 +0300

    changed path for redis to right host

--- mse/docker_settings.py
+++ mse/docker_settings.py
@@ -2,13 +2,13 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-CELERY_BROKER_URL = 'redis://localhost//'
+CELERY_BROKER_URL = 'redis://redis//'
 
 CELERY_RESULT_BACKEND = 'django-db'
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/1",
+        "LOCATION": "redis://redis:6379/1",
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
         }
